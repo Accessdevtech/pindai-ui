f3eacba58361dfd9cf1893a7478fe137d643b7f5
Farhan Reizha <48412599+farhanreizha@users.noreply.github.com>
2025-04-26 21:02:45 +0700

    fix(detail): penelitian and pengabdian add view surat keterangan selesai

--- src/modules/dppm/feature/penelitian/dppm.detail.penelitian.tsx
+++ src/modules/dppm/feature/penelitian/dppm.detail.penelitian.tsx
@@ -35,6 +35,7 @@ export default function DetailPenelitianDppmPage({ id }: { id: string }) {
   const [resDocs, setResDocs] = useState<{
     proposal?: ViewDocs
     laporanKemajuan?: LaporanKemajuan[]
+    suratKeteranganSelesai?: ViewDocs
     laporan?: ViewDocs
   }>()
   const [keterangan, setKeterangan] = useState("")
@@ -126,9 +127,11 @@ export default function DetailPenelitianDppmPage({ id }: { id: string }) {
     Promise.allSettled([
       viewFile("proposal"),
       viewFileLaporanKemajuan(),
+      viewFile("surat_keterangan_selesai"),
       viewFile("laporan"),
     ]).then(response => {
-      const [proposal, laporanKemajuan, laporan] = response
+      const [proposal, laporanKemajuan, suratKeteranganSelesai, laporan] =
+        response
       setResDocs({
         proposal: proposal.status === "fulfilled" ? proposal.value : undefined,
         laporanKemajuan:
@@ -136,6 +139,10 @@ export default function DetailPenelitianDppmPage({ id }: { id: string }) {
             ? Array.isArray(laporanKemajuan.value)
               ? laporanKemajuan.value
               : [laporanKemajuan.value]
+            : undefined,
+        suratKeteranganSelesai:
+          suratKeteranganSelesai.status === "fulfilled"
+            ? suratKeteranganSelesai.value
             : undefined,
         laporan: laporan.status === "fulfilled" ? laporan.value : undefined,
       })
@@ -325,8 +332,8 @@ export default function DetailPenelitianDppmPage({ id }: { id: string }) {
         </CardContent>
       </Card>
 
-      <div className='flex items-start gap-4'>
-        <Card className='shrink-0 grow'>
+      <div className='grid grid-cols-2 items-start gap-4'>
+        <Card>
           <CardHeader className='flex items-center justify-between p-6'>
             <CardTitle className='capitalize tracking-wide'>
               Laporan Kemajuan
@@ -378,11 +385,26 @@ export default function DetailPenelitianDppmPage({ id }: { id: string }) {
           </CardContent>
         </Card>
 
-        <Card
-          className={cn({
-            grow: !resDocs?.laporan,
-          })}
-        >
+        <Card>
+          <CardHeader className='flex items-center justify-between p-6'>
+            <CardTitle className='capitalize tracking-wide'>
+              Surat Keterangan Selesai
+            </CardTitle>
+          </CardHeader>
+          <CardContent className='space-y-2 p-6 capitalize text-muted-foreground'>
+            {isPending ? (
+              <Loader2Icon className='animate-spin' />
+            ) : resDocs?.suratKeteranganSelesai ? (
+              <FileView resDocs={resDocs?.suratKeteranganSelesai as ViewDocs} />
+            ) : (
+              <div className='capitalize'>
+                File tidak tersedia / belum di unggah
+              </div>
+            )}
+          </CardContent>
+        </Card>
+
+        <Card>
           <CardHeader className='flex items-center justify-between p-6'>
             <CardTitle className='capitalize tracking-wide'>Laporan</CardTitle>
           </CardHeader>
