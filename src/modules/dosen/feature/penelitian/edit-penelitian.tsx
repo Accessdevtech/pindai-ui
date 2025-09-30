ddb78be88c102f7259d2208556e8e7f91ea6f621
farhanreizha <reizha226@gmail.com>
2025-09-30 20:44:06 +0700

    refactor(academic-years): update academic year range generation
    Change academic year range from currentYear-5/currentYear+5 to currentYear/currentYear+10 in all research and community service forms to provide more future years for planning

--- src/modules/dosen/feature/penelitian/edit-penelitian.tsx
+++ src/modules/dosen/feature/penelitian/edit-penelitian.tsx
@@ -130,10 +130,7 @@ export default function EditPenelitian({ id }: EditPenelitianProps) {
 
   useEffect(() => {
     const currentYear = new Date().getFullYear()
-    const akademikYears = generateAcademicYears(
-      currentYear - 5,
-      currentYear + 5
-    )
+    const akademikYears = generateAcademicYears(currentYear, currentYear + 10)
     setTahunAkademik(akademikYears)
   }, [])
 
