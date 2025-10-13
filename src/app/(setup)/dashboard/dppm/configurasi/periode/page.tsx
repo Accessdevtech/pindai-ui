c3c8b4142f1e9c11dc4dae696cc4a8edfeed2ea8
farhanreizha <reizha226@gmail.com>
2025-10-14 01:45:20 +0700

    feat: add new pages for Dokument and Periode with user authentication

---
+++ src/app/(setup)/dashboard/dppm/configurasi/periode/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { useRequiredAuth } from "@/hooks/use-required-auth"
+import Periode from "@/modules/dppm/feature/configurasi/periode/periode"
+
+export default function () {
+  const { user } = useRequiredAuth()
+  if (!user) return null
+  return <Periode role={user.role} />
+}
